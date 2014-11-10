cd818fe17857748e418d367a09ed0bad5f015a0f
alekulyn <alekulyn@users.noreply.github.com>
2014-11-10 15:27:38 +0200

    fix s3cmd bucket slash error, directory change

--- dailymirror.sh
+++ dailymirror.sh
@@ -2,4 +2,4 @@
 
 BUCKET=""														# location of the bucket, FORMAT: s3://bucket-name
 FILES_DIR=""													# location of files
-s3cmd sync -r $FILES_DIR $BUCKET/files
+s3cmd sync -r $FILES_DIR/ $BUCKET/$(basename $FILES_DIR)/
